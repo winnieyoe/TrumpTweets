c142ad29a43021a8097c7efac2796ae62dadacca
Winnie Yoe <winnayoe@gmail.com>
2020-05-28 21:28:45 -0400

    responsive

--- silence.js
+++ silence.js
@@ -1,6 +1,7 @@
 //Trump tweets dataset from https://www.kaggle.com/austinreese/trump-tweets/data
 let table;
 let canvas;
+let twitterNotification;
 let tweet;
 let tweets = [];
 let synth;
@@ -13,10 +14,12 @@ let count = 0;
 
 function preload(){
   table = loadTable("trumptweets.csv", "csv", "header");
+  soundFormats('mp3', 'ogg');
+  twitterNotification = loadSound('assets/twitter.mp3');
 }
 
 function setup(){
-  canvas = createCanvas(windowWidth, windowHeight*0.7);
+  canvas = createCanvas(displayWidth, displayHeight);
   canvas.parent("visual");
 
   for (let i=0; i<table.getRowCount(); i++){
@@ -24,18 +27,6 @@ function setup(){
     let time = table.getRow(i).get("date").slice(11,16);
     tweet = table.getRow(i).get("content");
     word = tweet.split(" ").filter( (w) => w!==undefined && w.length > 0 ); //left: input of function, right: output of function
-    // result = [];
-    //console.log(word);
-    //for(let j=0; i<word.length; j++){
-      //console.log(word[j]);
-      // if(word[j] != null){
-      //   if(!word[j].includes("http")){
-      //     result.push(word[j]);
-      //   }
-      // }
-      //result.push(word[j]);
-    //}
-    //word = result;
 
     tweets[i] = {
       date: date,
@@ -45,16 +36,14 @@ function setup(){
       // wordsLength: getWordLengths(tweet),
       // haveSilence: haveSilence(tweet),
     }
-    // console.log(haveSilence(tweet))
   }
-  // console.log(tweets);
-  frameRate(1);
+  frameRate(10);
 
   synth = new Tone.Synth().toMaster();
 }
 
 function windowResized(){
-  canvas = resizeCanvas(windowWidth, windowHeight*0.7);
+  canvas = resizeCanvas(displayWidth, displayHeight);
 }
 
 submitForm.onsubmit = function(e){
@@ -63,53 +52,12 @@ submitForm.onsubmit = function(e){
   alert("Searching for '" + inputWord.toUpperCase() + "' in Trump Tweets");
   count = 0;
 }
-// submitForm.addEventListener("onsubmit", function(e){
-//   e.preventDefault();
-//   console.log(e)
-//   return false;
-// })
-
-// function getWordLengths(str){
-//   let len = [];
-//   let words = str.split(" ");
-//   // console.log(words);
-//   for (let i=0; i < words.length; i++){
-//     len.push(words[i].length);
-//   }
-//   return len;
-// }
-//
-// function haveSilence(str){
-//   let haveSilence = [];
-//   let words = str.toLowerCase().split(" ");
-//
-//   if (words.includes("silence")){
-//     console.log("yes")
-//     haveSilence.push("yes");
-//     // haveSilence.push(yes)
-//   } else {
-//     // console.log("no")
-//     haveSilence.push("no");
-//   }
-// }
 
 let i = 0;
 function draw(){
 background(21, 18, 169);
 textAlign(CENTER);
 textSize(16);
-  // for (let i=0; i<1; i++){
-  //  for(let j=0; j< tweets[0].word.length; j++){
-  //    console.log(tweets[0].word[2]);
-  //    if(tweets[i].word[j] == "be" || tweets[i].word[j] == "Be" || tweets[i].word[j] =="BE"){
-  //      fill(0);
-  //      text(tweets[i].word[j], width/2, (j+1)*30);
-  //    } else {
-  //      fill(255, 0, 0);
-  //      text(tweets[i].word[j], width/2, (j+1)*30);
-  //    }
-  //  }
-  // }
 
   if (i<tweets.length){
     for(let j=0; j< tweets[i].word.length; j++){
@@ -119,7 +67,8 @@ textSize(16);
       if(tweets[i].word[j].toLowerCase() == inputWord.toLowerCase()){
         fill(255, 255, 255);
         text(tweets[i].word[j], width/2, (j+1)*28);
-        synth.triggerAttackRelease("C4", "32n");
+        // synth.triggerAttackRelease("C4", "32n");
+        twitterNotification.play();
         count++;
       } else {
         let theWord = tweets[i].word[j];
